b7b83d5141106e0ca4046fac0a58b47c5b23337f
fraluegut <franciscojavierluengo@outlook.com>
2020-11-10 01:03:16 +0100

    Starting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:digital_print/src/pages/index_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,11 +7,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Digital Print App',
-      initialRoute: "index",
-      routes: {
-        "index": (BuildContext context) => IndexPage(),
-      },
-    );
+      title: 'Digital Print App1',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Digital Print APP"),
+        ),
+        body: Column(
+          children: <Widget>[
+          Padding(padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 5.0),),
+
+          Image(
+            image: new AssetImage('assets/images/digital_print.png')
+          ),
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  ButtonTheme(
+                  minWidth: 100.0,
+                  height: 50.0,
+                  child: RaisedButton(
+                    
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Empecemos', 
+                      style: new TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                    ),),
+                    shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                    
+                    color: Colors.white,
+                    splashColor: Colors.grey,
+                    
+                  ),
+                  ),
+                  Padding(padding: EdgeInsets.all(10.0),),
+                  ButtonTheme(
+              minWidth: 100.0,
+              height: 50.0,
+              child: RaisedButton(
+                
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text('Créditos', 
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.black,
+                ),),
+                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(20.0)),
+                
+                color: Colors.white,
+                splashColor: Colors.grey,
+                
+              ),
+              )
+                ],
+              ),
+              
+          
+          
+          ],)],
+        
+          
+      ),
+      
+    ));
+    
   }
 }
